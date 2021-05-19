6d2e72613c8e6876ae998ea9e8eeb14dc0c1d1fd
giannurwana19 <giannurwana19@gmail.com>
2021-05-19 10:36:56 +0700

    update link bootstrap v5.0.1 & menambahkan animasi

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const galleryImage = document.querySelectorAll('.gallery-img');
+
+galleryImage.forEach((img, index) => {
+  img.dataset.aos = 'fade-down';
+  img.dataset.aosDelay = index * 50;
+  img.dataset.aosDuration = 5000;
+});
+
+// init AOS
+AOS.init({
+  // once: true,
+  duration: 1000, // durasi default
+});
+
+// Gsap
+// register plugin text
+gsap.registerPlugin(TextPlugin);
+
+gsap.to('.lead', {
+  duration: 2,
+  text: {
+    value: 'Mahasiswa | Web Developer',
+  },
+  delay: 1.5,
+});
+
+gsap.from('.jumbotron img', {
+  duration: 1.2,
+  y: -100,
+  opacity: 0,
+  rotateY: 360,
+});
+
+gsap.from('.navbar', {
+  duration: 1,
+  y: '-100%',
+  opacity: 0,
+  ease: 'bounce',
+});
+
+gsap.from('.display-5', {
+  duration: 1,
+  x: 50,
+  opacity: 0,
+  delay: 0.5,
+  ease: 'back',
+});
+
+// Form Firebase
 const scriptURL =
   'https://script.google.com/macros/s/AKfycbxzKzkne28YNnF_uMdPIwJFzKrMfhY0hjgpy7I0KgYdCLacdsCLNtUeylXsZj8GTZsU/exec';
 const form = document.forms['submit-to-google-sheet'];
@@ -30,3 +79,8 @@ form.addEventListener('submit', e => {
 
 // e.preventDefault();
 // e.preventDefault() akan mengirimkan data dengan method get (fungsi default nya)
+
+const textYear = document.getElementById('textYear');
+const date = new Date();
+
+textYear.innerText = date.getFullYear();
